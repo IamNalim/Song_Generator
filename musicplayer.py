cce6407e6249e2193afc11c7bd5fd21f5c87a871
IamNalim <48150037+IamNalim@users.noreply.github.com>
2020-10-31 10:48:14 +0100

    added 0.5 version

--- musicplayer.py
+++ musicplayer.py
@@ -1,3 +1,5 @@
+# Author: Milan Matousek
+# Oct 2020
 import os 
 import tkinter as tk
 from tkinter import messagebox
@@ -9,21 +11,27 @@ global i
 global hraje
 global poprve
 global song_name
+global volume_value
+volume_value = 1
 song_name = '---'
 poprve = 1
 hraje = 1
-i = 2
+i = 1
 songs = []
 
 # function return print of all mp3 files in current dict
 def music_in_dir():
     global songs
     global i
+
+    #fix if first item in listbox is this str and someone added mp3
+    #this str remain as first item and song is add as second
+    if 'No music found' in song_list.get(0):
+        song_list.delete(0)
+
     for file in os.listdir():
         # check if file is mp3 and not already in our app
         if ('.mp3' in file) and (file not in songs):
-            if i == 2: # need for proper work with listbox
-                song_list.insert(1, 'music list:') #Title of listbox
             song_list.insert(i, file) #new item into listbox
             songs.append(file) #remember already claimed song
             print(songs)
@@ -65,23 +73,27 @@ def start_stop_func():
         print('3')
 
 def back_func():
-    #user choose valid song
     global song_name
     global poprve
     global hraje
 
     t = sum(song_list.curselection()) # tuple to int
 
-    if t <= 1:
-        print('dal nejdu')
+    if t == 0:
+        print('Zacatek seznamu')
+    elif t == 0 and len(songs) == 0:
+        print('No songs aviable')
     else:
-        #mixer.music.stop()
+        mixer.music.stop() #stop playing music
         #print('Muzeme zmenit song')
-        #hraje = 1
-        #poprve = 1
-        #song_name = song_list.get(t-1) # song before our current song
-        #current_play.configure(text='Current playing: '+song_name) #update label
-        #start_stop_func()
+        # setup global variables
+        hraje = 1 
+        poprve = 1
+        song_name = song_list.get(t-1) # song before our current song
+        current_play.configure(text='Current playing: '+song_name) #update label
+        print('----------'+song_name+'----------')
+        start_stop_func()
+        # this is very important! need to change index of item
         print('DEBUG')
         print(song_list.curselection())
         song_list.selection_clear(t)
@@ -91,7 +103,48 @@ def back_func():
         print('DEBUG')
 
 def forward_func():
-    pass
+    global song_name
+    global poprve
+    global hraje
+
+    if song_list.size() == 0:
+        #no item in list
+        print('No item')
+        return
+    elif song_list.size() == 1 and song_list.get(0) == 'No music found':
+        #no music found listbox
+        print('No song in dict')
+        return
+    else:
+        # continue work
+        t = sum(song_list.curselection()) # tuple to int
+        # lets fix if listbox item is choosen
+        try:
+            song_list.get(song_list.curselection()[0])
+        except IndexError as e:
+            print('No song choosen ({0})'.format(e))
+            return
+        # fint out if we are not at last song item
+        if t < song_list.size() - 1:
+            print('Muzu posunout')
+            mixer.music.stop() # stop current playing music
+            poprve = 1
+            hraje = 1
+            song_name = song_list.get(t+1) # song before our current song
+            current_play.configure(text='Current playing: '+song_name) #update label
+            start_stop_func()
+            # this is very important! need to change index of item
+            print('DEBUG')
+            print(song_list.curselection())
+            song_list.selection_clear(t)
+            print(song_list.curselection())
+            song_list.selection_set(t+1)
+            print(song_list.curselection())
+            print('DEBUG')
+        else:
+            #cant move forward
+            print('Cant move forward')
+            return
 
 # function for helping how to workds with music player
 def help_func():
@@ -101,22 +154,41 @@ Click on button for find songs then pick them and play'''
 
 #func for choosing music
 def klik(param):
-    #print(param)
-    #print(song_list.curselection()[0])
-    #print(song_list.get(song_list.curselection()[0]))
     global song_name
     global poprve
     global hraje
 
-    #user choose valid song
-    if song_list.curselection()[0] == 0 and song_name == '---':
-        pass
-    else: 
-        mixer.music.stop() # we need to cutoff old song
-        hraje = 1 # reset this one
-        poprve = 1 # first play of song
-        song_name = song_list.get(song_list.curselection()[0]) #new name
-        current_play.configure(text='Current playing: '+song_name) #update label
+    #if there is song in listbox
+    if song_list.get(song_list.curselection()[0]) != 'No music found':
+        mixer.music.stop() # stop song before
+        # reset global variables
+        hraje = 1
+        poprve = 1
+        # setup new song name
+        song_name = song_list.get(song_list.curselection()[0])
+        # setup current song playing label for info
+        current_play.configure(text='Current playing: '+song_name)
+        start_stop.configure(text='|>') # just in case for GUI fixes
+
+# change volume to up
+def volumeChange(up=1):
+    global volume_value
+    
+    print(mixer.music.get_volume())
+    if up == 1:
+        print('Zvysujeme')
+        volume_value = volume_value + 0.1
+        if volume_value > 1:
+            volume_value = 1
+        print(volume_value)
+        mixer.music.set_volume(volume_value)
+    else:
+        print('Snizujeme')
+        volume_value = volume_value - 0.1
+        if volume_value < 0:
+            volume_value = 0
+        print(volume_value)
+        mixer.music.set_volume(volume_value)
 
 # main loop of app
 def main():
@@ -151,6 +223,14 @@ start_stop.place(x=30, y=30)
 forward = tk.Button(root, text='>|')
 forward.configure(command=forward_func)
 forward.place(x=50, y=30)
+
+# volume
+vol_down = tk.Button(root, text='- ')
+vol_down.configure(command=lambda:volumeChange(0))
+vol_down.place(x=75, y=30)
+vol_up = tk.Button(root, text='+')
+vol_up.configure(command=volumeChange)
+vol_up.place(x=95, y=30)
 
 #button for show all mp3 files in currend dict
 current_dict = tk.Button(root, text='Search in dict')
